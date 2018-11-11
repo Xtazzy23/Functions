1faabc46b82c2be0e002f26de91590b3d6ff8412
Xtazzy23 <44172780+Xtazzy23@users.noreply.github.com>
2018-11-11 20:19:34 +0530

    Initial commit

---
+++ Functions.py
@@ -0,0 +1,7 @@
+def say_hi (name, age):
+# defining function giving prompt as say_hi space (variable) without inverted commas
+    print("Hello " + name + " you are " + str(age))
+# maintain same indent to print "HCS" + variable + "HCS" + variable with string
+say_hi("Sanjay", 50)
+# print variable name & age to deliver the message
+say_hi("Sanu", 15)
